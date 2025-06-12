617f1b500b4df8d01f9030ef4a9f994cfe0f9eea
Shivam Tiwari <shivamt765@gmail.com>
2025-06-12 15:02:56 +0530

    new

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
+// vite.config.ts or vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: './', // 👈 This fixes white screen on Render or nested paths
 });
